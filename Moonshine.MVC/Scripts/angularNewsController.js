587b63bc7e77276a9efb79567ffdf9b7d3984ed1
pouli <paul.deblicker@isen-lille.fr>
2014-11-15 21:06:42 +0100

    Add angular, bootstrap, ...

---
+++ Moonshine.MVC/Scripts/angularNewsController.js
@@ -0,0 +1,21 @@
+﻿var newsApp = angular.module("newsApp", [])
+
+newsApp.factory('newsFactory', function ($http) {
+    return {
+        getFormData: function (callback) {
+            $http.get('/api/news').success(callback);
+        }
+    }
+})
+
+newsApp.controller("newsController", function ($scope, newsFactory) {
+    getFormData();
+    function getFormData() {
+        newsFactory.getFormData(function (results) {
+            $scope.news = results.news;
+        })
+    }
+    $scope.Save = function () {
+        $scope.message = "News Data Submitted"
+    }
+})
